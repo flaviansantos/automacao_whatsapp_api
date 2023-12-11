b9d257c667f75e2b1cc9db39736cb6d98a899b94
flaviansantos <flavia.neves1984@outlook.com>
2023-12-11 02:00:43 -0300

    Api

---
+++ Api_BotWhatsApp.Api/Controllers/WhatsAppController.cs
@@ -0,0 +1,45 @@
+﻿using Api_BotWhatsApp.Api.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api_BotWhatsApp.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WhatsAppController : ControllerBase
+    {
+        private readonly ILogger<WhatsAppController> _logger;
+
+        public WhatsAppController(ILogger<WhatsAppController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("GetMessageAVencer")]
+        public JsonResult GetMessageAVencer()
+        {
+            List<Model.MensagensWhatsApp> list =
+            [
+                new MensagensWhatsApp
+                {
+                    nome = "Bruno Lucas",
+                    telefone = "5519999999999",
+                    mensagem = "Sr. Bruno, o vencimento da sua fatura é 11/12/2023."
+                },
+                new MensagensWhatsApp
+                {
+                    nome = "Flávia Neves",
+                    telefone = "5519999999999",
+                    mensagem = "Sra. Flávia, o vencimento da sua fatura é 15/12/2023."
+                },
+                new MensagensWhatsApp
+                {
+                    nome = "Diogo Brandão",
+                    telefone = "5519999999999",
+                    mensagem = "Sr. Diogo, o vencimento da sua fatura é 15/12/2023."
+                },
+            ];
+
+            return new JsonResult(list);
+        }
+    }
+}
---
+++ Api_BotWhatsApp.Api/Model/MensagensWhatsApp.cs
@@ -0,0 +1,9 @@
+﻿namespace Api_BotWhatsApp.Api.Model
+{
+    public class MensagensWhatsApp
+    {
+        public string telefone { get; set; }
+        public string nome { get; set; }
+        public string mensagem { get; set; }
+    }
+}
